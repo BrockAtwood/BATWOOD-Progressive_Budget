37c6e8ed5f267eb3bfa5036a7c4d317e67a8ccbf
Brock Atwood <brock.atwood@du.edu>
2021-09-29 22:26:23 -0600

    added comments and process.env to port and connection for mongo

--- server.js
+++ server.js
@@ -1,28 +1,37 @@
+//given file
+//dependencies from package.json
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+//PORT boiler for heroku later
+const PORT = process.env.PORT || 3131;
 
+//express app
 const app = express();
 
+//Middleare for morgan dependency
 app.use(logger("dev"));
 
+// Middleware for compression, parsing JSON and urlencoded form data
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
+//app listener for connection
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
