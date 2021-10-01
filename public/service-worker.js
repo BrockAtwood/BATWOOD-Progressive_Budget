7d39975f5fee9e1479bd752bd1389ac8d38fde2a
Brock Atwood <brock.atwood@du.edu>
2021-10-01 08:18:07 -0600

    fixing a bug with instructor

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,6 +9,7 @@ const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
+  "/index.js",
   "/indexedDB.js",
   "/manifest.webmanifest",
   "/icons/icon-192x192.png",
